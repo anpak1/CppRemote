204113e594a8d219a48e3cff58335f8ae99fc6fb
anpak1 <andrey.pakosh@gmail.com>
2015-07-19 01:25:50 +0300

    trying to implement remote interface

---
+++ build/CMakeFiles/remote.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anp/CppRemote/src/remote/archive/binary_iarchive.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/archive/binary_iarchive.cpp.o"
+  "/home/anp/CppRemote/src/remote/archive/text_iarchive.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/archive/text_iarchive.cpp.o"
+  "/home/anp/CppRemote/src/remote/archive/xml_iarchive.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/archive/xml_iarchive.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/buffer_pool.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/buffer_pool.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/local_acceptor.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/local_acceptor.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/local_transport.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/local_transport.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/named_pipe_acceptor.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/named_pipe_acceptor.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/named_pipe_transport.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/named_pipe_transport.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/tcp_acceptor.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/tcp_acceptor.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/tcp_endpoint.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/tcp_endpoint.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/tcp_transport.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/tcp_transport.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/udp_acceptor.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/udp_acceptor.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/udp_endpoint.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/udp_endpoint.cpp.o"
+  "/home/anp/CppRemote/src/remote/bindings/udp_transport.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/bindings/udp_transport.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/call.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/call.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/comm_link.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/comm_link.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/direct_link.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/direct_link.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/pool_link.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/pool_link.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/pool_node.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/pool_node.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/proxy.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/proxy.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/proxy_pool.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/proxy_pool.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/result.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/result.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/server.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/server.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/service_pool.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/service_pool.cpp.o"
+  "/home/anp/CppRemote/src/remote/detail/type_id.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/detail/type_id.cpp.o"
+  "/home/anp/CppRemote/src/remote/exception.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/exception.cpp.o"
+  "/home/anp/CppRemote/src/remote/future_error.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/future_error.cpp.o"
+  "/home/anp/CppRemote/src/remote/global.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/global.cpp.o"
+  "/home/anp/CppRemote/src/remote/io_runner.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/io_runner.cpp.o"
+  "/home/anp/CppRemote/src/remote/pool_node.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/pool_node.cpp.o"
+  "/home/anp/CppRemote/src/remote/register_archive.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/register_archive.cpp.o"
+  "/home/anp/CppRemote/src/remote/serialization/system/error_categories.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/serialization/system/error_categories.cpp.o"
+  "/home/anp/CppRemote/src/remote/server.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/server.cpp.o"
+  "/home/anp/CppRemote/src/remote/session.cpp" "/home/anp/CppRemote/build/CMakeFiles/remote.dir/src/remote/session.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
